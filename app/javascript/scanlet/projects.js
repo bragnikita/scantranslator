de83362d20ea7d241c09a191f8e13edd02d8f4d4
bragnikita <bragnikita@gmail.com>
2017-12-10 08:59:44 +0900

    dropzone test

--- app/javascript/scanlet/projects.js
+++ app/javascript/scanlet/projects.js
@@ -1,4 +1,5 @@
 import ContextForm from '../context_forms/context-forms.js'
+import Dropzone from 'dropzone'
 
 $(document).on('turbolinks:load', function () {
     'use strict';
@@ -6,5 +7,21 @@ $(document).on('turbolinks:load', function () {
     const form = new ContextForm('#project-description-form');
     form.initComponents();
 
+    const project_cover = $("#project-description").find(".cover");
+    project_cover.addClass('dropzone');
+
+    const dz = new Dropzone("#project-description .cover", {
+        url: "uploaded_images",
+        paramName: $(project_cover).data('upload-param') || 'file',
+        createImageThumbnails: true,
+        autoProcessQueue: false,
+        maxFiles: 2,
+        clickable: "#project-description .cover, #project-description .cover .current",
+        acceptedFiles: 'image/*'
+
+    });
+    dz.on("addedfile", (f) => {
+        project_cover.find('img.current').hide();
+    })
 
 });
---
+++ app/javascript/scanlet/scanlet.js
@@ -0,0 +1,4 @@
+import '../scanlet/projects.scss'
+import '../scanlet/groups.scss'
+import '../scanlet/layout.scss'
+import '../scanlet/projects.js'
